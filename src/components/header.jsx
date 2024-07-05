847f6f419b4ae29df2f660da4b166e0dafae41f2
anonscodex <normalstuff2001@mail.com>
2024-07-05 15:31:41 +0100

    change background

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -19,14 +19,14 @@ const Header = () => {
         a place for skilled service provider</h2>
         <form className="space-y-4" onSubmit={handleSubmit}>
           <div>
-            <label htmlFor="email" className="block text-sm font-medium text-gray-300">
+            <label htmlFor="email" className="block text-sm font-medium text-gray-700">
               Email Address
             </label>
             <input
               type="email"
               id="email"
               name="email"
-              className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              className="mt-1 p-3 block w-full border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
               placeholder="you@example.com"
               required
             />
